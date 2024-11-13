4a71e8e05d2f19ad92a8f448dac96fadf178dabd
narsis9696 <narsis96@gmail.com>
2024-11-13 14:45:23 +0900

    열혈강의

---
+++ Cpp/Hardboil/pt_02_01_01.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+int main(void)
+{
+    printf("홍\n길동\n홍길동\n");
+    return 0; // return 명령어 철자 틀려 오류 주의
+}
---
+++ Cpp/Hardboil/pt_02_01_02.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main(void)
+{
+    printf("Name: 상현\n");
+    printf("Address : 서울\n");
+    printf("Phone Number: 000-0000-0000\n");
+    return 0;
+}
