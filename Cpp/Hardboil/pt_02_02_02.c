b83cddc8d56fba8b61b116ff93b2df785ef99c83
narsis9696 <narsis96@gmail.com>
2024-11-14 07:54:52 +0900

    주석 수정

--- Cpp/Hardboil/pt_02_02_02.c
+++ Cpp/Hardboil/pt_02_02_02.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 int main(void)
 {
-    printf("%dX%d=%d\n", 2, 3, 2 * 3); // 직접 계산하게 만드는거 가능
+    printf("%dX%d=%d\n", 2, 3, 2 * 3); // 직접 계산하게 만드는거 가능, 연산 우선후 함수에 인자 전달
     printf("%dX%d=%d\n", 2, 4, 2 * 4); // 직접 계산하게 만드는거 가능
     return 0;
 }
