1cb44843127c783efc83be1b29163286c5824749
narsis9696 <narsis96@gmail.com>
2024-11-16 12:16:04 +0900

    241116

---
+++ Cpp/C_Primer_Plus/Listing/list_2_5.c
@@ -0,0 +1,15 @@
+/*stillbad.c -- a program with its syntax errors(list 2.4) fixed*/
+#include <stdio.h>
+int main(void)
+{
+    int n, n2, n3;
+
+    /*this program has a semantic error*/
+    n = 5;
+    n2 = n * n;
+    n3 = n2 * n2;
+
+    printf("n= %d, n squared = %d, n cubed = %d\n", n, n2, n3); // semantic error: cube -> fourth power of n
+
+    return 0;
+}
