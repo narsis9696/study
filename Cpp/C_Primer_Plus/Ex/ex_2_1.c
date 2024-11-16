835d43716993deaddc864468ad4ef342904d2e72
narsis9696 <narsis96@gmail.com>
2024-11-16 17:14:10 +0900

    연습문제 1번

---
+++ Cpp/C_Primer_Plus/Ex/ex_2_1.c
@@ -0,0 +1,12 @@
+/*Ch2 Exercise 1*/
+
+#include <stdio.h>
+int main(void)
+{
+    printf("Gustav Mahler\n");
+    printf("Gustav\nMahler\n");
+    printf("Gustav ");
+    printf("Mahler\n");
+
+    return 0;
+}
