f4b80f8e4aaab605e57f1e80ee26a78d767fa9cc
narsis9696 <narsis96@gmail.com>
2024-11-22 00:45:01 +0900

    up

--- Cpp/C_Primer_Plus/Listing/list_3_7.c
+++ Cpp/C_Primer_Plus/Listing/list_3_7.c
@@ -4,7 +4,8 @@ int main(void)
 {
     float aboat = 32000.0;
     double abet = 2.14e9;
-    long double dip = 5.32e-5;
+    double dip = 5.32e-5; /*교재는 long double 로 되어 있지만 error난다.
+     mingw랑 맞지 않아서 문제 발생이라고 생각됨. double 로 하면 정상 작동한다.*/
 
     printf("%f can be written %e\n", aboat, aboat);
     // next line requires C99 or later compliance
