8e181ef88077d2d38b7d3096f79a537779e3fa91
narsis9696 <narsis96@gmail.com>
2025-07-09 14:52:12 +0900

    주석에 한글

--- Cpp/C_Primer_Plus/Listing/list_3_1.c
+++ Cpp/C_Primer_Plus/Listing/list_3_1.c
@@ -14,7 +14,7 @@ int main(void)
     /*assume platinum is $1700 per ounce*/
     /*14.5833 converts pounds avd. to ounces troy*/
     value = 1700.0 * weight * 14.5833;
-    printf("your weight in platinum is worth $%.2f.\n", value);
+    printf("your weight in platinum is worth $%.2f.\n", value); /*소수점 둘째자리까지 출력*/
     printf("You are easily worth that! If platinum prices drop,\n");
     printf("eat more to maintain your value.\n");
 
