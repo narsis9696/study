58b8b4dce296c3a7b08e9b35e2cbaff2628de09e
narsis9696 <narsis96@gmail.com>
2025-07-11 22:52:34 +0900

    잘 모르지만 커밋

---
+++ Cpp/CPP_Primer_Plus/Listing/List_2_1.cpp
@@ -0,0 +1,11 @@
+// mufirst.cpp -- 메세지를 출력한다
+
+#include <iostream>
+int main ()
+{
+    using namespace std;
+    cout << "C++의 세계로 오십시오.";
+    cout << endl;
+    cout << "후회하지 않으실 겁니다!" <<endl;
+    return 0;
+}
