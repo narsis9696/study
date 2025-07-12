15b4dcb5ce47966b6f4f00aa12929ba4c3d7ae4f
narsis9696 <narsis96@gmail.com>
2025-07-12 15:04:31 +0900

    List_2_2.cpp 만들기 ready

---
+++ Cpp/CPP_Primer_Plus/Listing/List_2_2.cpp
@@ -0,0 +1,20 @@
+// carrots.cpp --
+// 
+
+#include <iostream>
+
+int main()
+{
+	using namespace std;
+
+	int carrots;
+
+	carrots = 25;
+	cout << "";
+	cout << carrots;
+	cout << "";
+	cout << endl;
+	carrot = carrots -1;
+	cout << "" << carrots << "" <<endl;
+	return 0;
+}
