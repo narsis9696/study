485b9ff9e1adc4eabd05667bb35ef97c322c2143
narsis9696 <narsis96@gmail.com>
2025-07-13 14:25:34 +0900

    변수, 띄어쓰기 수정

--- Cpp/CPP_Primer_Plus/Listing/List_2_2.cpp
+++ Cpp/CPP_Primer_Plus/Listing/List_2_2.cpp
@@ -10,11 +10,11 @@ int main()
 	int carrots;
 
 	carrots = 25;
-	cout << "나는 당근을";
+	cout << "나는 당근을 ";
 	cout << carrots;
 	cout << "개 가지고 있다.";
 	cout << endl;
-	carrot = carrots -1;
+	carrots = carrots -1;
 	cout << "아삭아삭, 이제 당근은 " << carrots << "개이다." <<endl;
 	return 0;
 }
