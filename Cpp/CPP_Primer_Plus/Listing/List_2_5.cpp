b2e6c5aef40590d21a0bcd183219beb5b07d5354
narsis9696 <narsis96@gmail.com>
2025-07-14 17:06:27 +0900

    주석

--- Cpp/CPP_Primer_Plus/Listing/List_2_5.cpp
+++ Cpp/CPP_Primer_Plus/Listing/List_2_5.cpp
@@ -17,5 +17,5 @@ int main()
 void simon(int n)   //simon() 함수를 정의한다
 {
     using namespace std;
-    cout << "Simon 왈, 발가락을 " <<n<< "번 두드려라." <<endl;
+    cout << "Simon 왈, 발가락을 " <<n<< "번 두드려라." <<endl; // 일부러 화이트스페이스 안주고 변수 n을 적었다
 }   //void형 함수에느 return 구문이 필요 없다
